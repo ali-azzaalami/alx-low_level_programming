786068389b4090ebed27212eaf18ebe46e71a597
ali-azzaalami <azzaalami100@gmail.com>
2023-07-21 15:40:26 -0700

    0x02

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * add - a function that adds two integers and returns the result.
- * @a: a paramter
- * @b: a paramter
- * Return: result of two integers
-*/
-int add(int a, int b)
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: a parameter
+ */
+void print_to_98(int n)
 {
-	int res;
 
-	res = a + b;
-
-	return (res);
+	if (n > 98)
+	{
+		for (n = n; n  > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	} else if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", 98);
+	} else
+	{
+		printf("%d\n", 98);
+	}
 }
